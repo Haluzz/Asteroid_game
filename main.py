3a3c3d99864594239851c09601183f512b8af1f0
Nyrew <lukas.vokel@gmail.com>
2024-09-30 21:17:09 +0200

    correction of the base screen

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import pygame
 from constants import *
 
 def main():
-    # print("Starting asteroids!")
-    # print(f"Screen width: {SCREEN_WIDTH}")
-    # print(f"Screen height: {SCREEN_HEIGHT}")
-
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     running = True
+    
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
